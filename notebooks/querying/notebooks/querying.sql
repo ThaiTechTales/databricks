a2a6bd901f5c1968edac52ae3e89f0bcdb6aaecb
ThaiTechTales <ThaiTechTales>
2025-01-17 11:43:57 +0000

    feat: write queries

--- notebooks/querying/notebooks/querying.sql
+++ notebooks/querying/notebooks/querying.sql
@@ -1,14 +1,20 @@
 -- Databricks notebook source
 -- MAGIC %md
 -- MAGIC # Querying Files with Spark SQL
+
 -- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC ## JSON Data
+
 -- COMMAND ----------
+
 -- MAGIC %python
 -- MAGIC files = dbutils.fs.ls("file:/Workspace/Users/<user>/databricks/notebooks/querying-ecommerce/data/transactions-csv/")
 -- MAGIC display(files)
+
 -- COMMAND ----------
+
 -- File contents queried directly using Spark SQL
 SELECT
     *
@@ -16,6 +22,7 @@ FROM
     json.`file:/Workspace/Users/<user>/databricks/notebooks/querying-ecommerce/data/customers-json/customers_001.json`;
 
 -- COMMAND ----------
+
 -- Multiple files queried directly using Spark SQL
 -- Select all columns
 SELECT
@@ -24,6 +31,7 @@ FROM
     json.`file:/Workspace/Users/<user>/databricks/notebooks/querying-ecommerce/data/customers-json/customers_*.json`;
 
 -- COMMAND ----------
+
 -- Count the total number of records in the JSON files located at the specified path 
 -- Record: row in a table
 -- COUNT(*) counts the number of records in the JSON files
@@ -34,6 +42,7 @@ FROM
     json.`file:/Workspace/Users/<user>/databricks/notebooks/querying-ecommerce/data/customers-json`;
 
 -- COMMAND ----------
+
 SELECT
     *, -- Select all columns from the JSON files
     input_file_name () AS source_file -- Add a new column 'source_file' that contains the name of the file from which each record was read
@@ -42,10 +51,14 @@ FROM
     json.`file:/Workspace/Users/<user>/databricks/notebooks/querying-ecommerce/data/customers-json/*.json`;
 
 -- Specify the path to the JSON files
+
 -- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC ## Text Data
+
 -- COMMAND ----------
+
 -- Reads from the text file
 SELECT
     *
@@ -53,18 +66,24 @@ FROM
     text.`file:/Workspace/Users/<user>/databricks/notebooks/querying-ecommerce/data/customers-json/customers_003.txt`;
 
 -- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC ## Binary Data
+
 -- COMMAND ----------
+
 SELECT
     *
 FROM
     binaryFile.`file:/Workspace/Users/<user>/databricks/notebooks/querying-ecommerce/data/customers-json/customers_004.bin`;
 
 -- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC ## CSV Data
+
 -- COMMAND ----------
+
 -- Reads a CSV files
 SELECT
     *
@@ -72,12 +91,12 @@ FROM
     csv.`file:/Workspace/Users/<user>/databricks/notebooks/querying-ecommerce/data/transactions-csv/`;
 
 -- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC ## CTAS
+
 -- COMMAND ----------
--- The temporary view serves as a staging layer for transformations, options handling, and schema validation.
--- Loading data into a Delta table from a temporary view ensures you get the full benefits of Delta Lake features while maintaining data integrity and performance.
--- Directly referencing external files skips this staging step, risking slower queries and limited functionality.
+
 CREATE TABLE
     customers_01 AS
 SELECT
@@ -88,6 +107,10 @@ FROM
 DESCRIBE EXTENDED customers_01;
 
 -- COMMAND ----------
+
+-- The temporary view serves as a staging layer for transformations, options handling, and schema validation.
+-- Loading data into a Delta table from a temporary view ensures you get the full benefits of Delta Lake features while maintaining data integrity and performance.
+-- Directly referencing external files skips this staging step, risking slower queries and limited functionality.
 CREATE TEMP VIEW transactions_tmp_vw_02 (
     transaction_id STRING,
     customer_id STRING,
@@ -110,9 +133,12 @@ FROM
 DESCRIBE EXTENDED transactions_delta_02;
 
 -- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC ## Data Refresh
+
 -- COMMAND ----------
+
 -- Add a new file to the directory manually or programmatically
 REFRESH TABLE transactions_delta_02;
 
--- notebooks/writing/notebooks/writing.sql
+++ notebooks/writing/notebooks/writing.sql
@@ -3,59 +3,65 @@
 -- MAGIC # Comprehensive Learning of Writing to Delta Tables with ACID Transactions Using JSON Files
 
 -- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC ## Step 1: Set Up Directories and Import Data
 -- MAGIC Ensure JSON files are located in the directory:  
--- MAGIC `file:/Workspace/Users/thai.le.trial.02@gmail.com/databricks/notebooks/writing/data/<folder>/`
+-- MAGIC `file:/Workspace/Users/thai.le.trial.02@gmail.com/databricks/notebooks/writing/data/`
 
 -- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC ## Step 2: Create Delta Tables
 -- MAGIC Create `orders` Delta table from JSON.
 
 -- COMMAND ----------
--- MAGIC %sql
-CREATE TABLE
-    orders USING DELTA AS
+
+drop table orders
+
+-- COMMAND ----------
+
+CREATE TABLE orders AS
 SELECT
     *
 FROM
     json.`file:/Workspace/Users/thai.le.trial.02@gmail.com/databricks/notebooks/writing/data/orders.json`;
 
 -- COMMAND ----------
--- MAGIC %sql
--- MAGIC Verify the data.
+
+-- Verify the data.
 SELECT
     *
 FROM
     orders;
 
 -- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC ## Step 3: Overwriting Data with CREATE OR REPLACE TABLE
 -- MAGIC Replace `orders` table data completely using CREATE OR REPLACE TABLE.
 
 -- COMMAND ----------
--- MAGIC %sql
-CREATE
-OR REPLACE TABLE orders USING DELTA AS
+
+CREATE OR REPLACE TABLE orders AS
 SELECT
     *
 FROM
     json.`file:/Workspace/Users/thai.le.trial.02@gmail.com/databricks/notebooks/writing/data/orders.json`;
 
 -- COMMAND ----------
--- MAGIC %sql
--- MAGIC Check table history.
+
+-- Check table history.
 DESCRIBE HISTORY orders;
 
 -- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC ## Step 4: Overwriting Data with INSERT OVERWRITE
 -- MAGIC Overwrite `orders` table data using INSERT OVERWRITE.
 
 -- COMMAND ----------
--- MAGIC %sql
+
 INSERT OVERWRITE orders
 SELECT
     *
@@ -63,25 +69,26 @@ FROM
     json.`file:/Workspace/Users/thai.le.trial.02@gmail.com/databricks/notebooks/writing/data/orders-new.json`;
 
 -- COMMAND ----------
--- MAGIC %sql
--- MAGIC Verify the data.
+
+-- Verify the data.
 SELECT
     *
 FROM
     orders;
 
 -- COMMAND ----------
--- MAGIC %sql
--- MAGIC Check table history.
+
+-- Check table history.
 DESCRIBE HISTORY orders;
 
 -- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC ### Attempt Schema Mismatch Scenario with INSERT OVERWRITE
 -- MAGIC Attempt to insert data with a mismatched schema.
 
 -- COMMAND ----------
--- MAGIC %sql
+
 INSERT OVERWRITE orders
 SELECT
     *,
@@ -90,12 +97,13 @@ FROM
     json.`file:/Workspace/Users/thai.le.trial.02@gmail.com/databricks/notebooks/writing/data/orders.json`;
 
 -- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC ## Step 5: Appending Data
 -- MAGIC Append new records to `orders` table.
 
 -- COMMAND ----------
--- MAGIC %sql
+
 INSERT INTO
     orders
 SELECT
@@ -104,53 +112,119 @@ FROM
     json.`file:/Workspace/Users/thai.le.trial.02@gmail.com/databricks/notebooks/writing/data/orders-new.json`;
 
 -- COMMAND ----------
--- MAGIC %sql
--- MAGIC Check the number of records.
+
+-- Check the number of records.
 SELECT
     COUNT(*)
 FROM
     orders;
 
 -- COMMAND ----------
+
+-- Verify the data.
+SELECT
+    *
+FROM
+    orders;
+
+-- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC ## Step 6: Merging Data
 -- MAGIC Create or Replace Temporary View for customer updates.
 
 -- COMMAND ----------
--- MAGIC %sql
-CREATE
-OR REPLACE TEMP VIEW customers_updates AS
+
+-- This temporary view is used to stage the data from the JSON file so that it can be easily accessed and manipulated in subsequent SQL operations without repeatedly reading the file.
+CREATE OR REPLACE TEMP VIEW customers_updates AS
 SELECT
     *
 FROM
     json.`file:/Workspace/Users/thai.le.trial.02@gmail.com/databricks/notebooks/writing/data/customers-updates.json`;
 
 -- COMMAND ----------
--- MAGIC %sql
--- MAGIC Merge data into `customers` table.
-MERGE INTO customers c USING customers_updates u ON c.customer_id = u.customer_id WHEN MATCHED
-AND c.email IS NULL
-AND u.email IS NOT NULL THEN
-UPDATE
-SET
-    email = u.email,
-    updated = u.updated WHEN NOT MATCHED THEN INSERT *;
+
+DROP TABLE IF EXISTS customers;
 
 -- COMMAND ----------
--- MAGIC %sql
--- MAGIC Verify the changes.
+
+CREATE TABLE IF NOT EXISTS
+    customers AS
+SELECT
+    *
+FROM
+    json.`file:/Workspace/Users/thai.le.trial.02@gmail.com/databricks/notebooks/writing/data/customers.json`;
+
+DESCRIBE EXTENDED customers;
+
+-- COMMAND ----------
+
+-- Verify the data.
 SELECT
     *
 FROM
     customers;
 
 -- COMMAND ----------
+
+-- This MERGE operation ensures that the customers table is updated with new email addresses and timestamps from the customers_updates view, and new records are inserted if they do not already exist in the customers table.
+
+-- WHEN MATCHED: This clause updates existing records in the customers table if a match is found based on customer_id and additional conditions.
+-- WHEN NOT MATCHED: This clause inserts new records into the customers table if no match is found based on customer_id.
+-- This logic ensures that each customer_id is unique in the customers table, thus preventing duplicates.
+
+-- Merge data into `customers` table.
+-- MERGE INTO customers c: 
+    -- This specifies the target table for the merge operation, which is customers.
+    -- The alias c is used to reference the customers table.
+MERGE INTO customers c 
+
+-- USING customers_updates u:
+    -- This specifies the source of the data for the merge operation, which is the customers_updates temporary view.
+    -- The alias u is used to reference the customers_updates view.
+
+-- ON c.customer_id = u.customer_id:
+    -- This specifies the condition for matching records between the customers table and the customers_updates view.
+    -- Records are matched based on the customer_id column.
+USING customers_updates u ON c.customer_id = u.customer_id 
+
+-- WHEN MATCHED AND c.email IS NULL AND u.email IS NOT NULL THEN
+    -- This part of the MERGE statement specifies the condition for updating records in the target table (customers) when a match is found.
+
+    -- WHEN MATCHED: This indicates that the following actions should be taken when a record in the customers table matches a record in the customers_updates view based on the ON condition specified earlier.
+    -- AND c.email IS NULL AND u.email IS NOT NULL: This adds an additional condition to the WHEN MATCHED clause. It specifies that the update should only occur if the email in the customers table is NULL and the email in the customers_updates view is not NULL.
+WHEN MATCHED AND c.email IS NULL AND u.email IS NOT NULL THEN
+
+-- UPDATE SET email = u.email, last_updated = u.last_updated:
+    -- This specifies the update action to be performed when the match condition is met.
+    -- The email and last_updated columns in the customers table are updated with the corresponding values from the customers_updates view.
+UPDATE SET
+    email = u.email,
+    last_updated = u.last_updated 
+
+-- WHEN NOT MATCHED THEN INSERT:
+    -- This specifies the action to be performed when there is no match between the customers table and the customers_updates view.
+    -- The INSERT * statement inserts the entire record from the customers_updates view into the customers table.
+WHEN NOT MATCHED THEN 
+
+INSERT *;
+
+-- COMMAND ----------
+
+-- Verify the changes.
+SELECT
+    *
+FROM
+    customers;
+
+-- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC ## Step 7: Conditional Merge with Specific Criteria
 -- MAGIC Create a temporary view for new book updates.
 
 -- COMMAND ----------
--- MAGIC %sql
+
 CREATE
 OR REPLACE TEMP VIEW books_updates (
     book_id STRING,
@@ -163,23 +237,46 @@ OR REPLACE TEMP VIEW books_updates (
 );
 
 -- COMMAND ----------
--- MAGIC %sql
--- MAGIC Preview the new data.
+
+SELECT 
+  *
+FROM books_updates
+
+-- COMMAND ----------
+
+DROP TABLE IF EXISTS books
+
+-- COMMAND ----------
+
+CREATE TABLE IF NOT EXISTS
+    books AS
 SELECT
     *
 FROM
-    books_updates;
+    json.`file:/Workspace/Users/thai.le.trial.02@gmail.com/databricks/notebooks/writing/data/books.json`;
+
+DESCRIBE books;
 
 -- COMMAND ----------
--- MAGIC %sql
--- MAGIC Merge into the `books` table only if the category is 'Computer Science'.
-MERGE INTO books b USING books_updates u ON b.book_id = u.book_id
+
+-- Preview the new data.
+SELECT
+    *
+FROM
+    books;
+
+-- COMMAND ----------
+
+-- Merge into the `books` table only if the category is 'Programming'.
+MERGE INTO books b 
+
+USING books_updates u ON b.book_id = u.book_id
 AND b.title = u.title WHEN NOT MATCHED
-AND u.category = 'Computer Science' THEN INSERT *;
+AND u.category = 'Programming' THEN INSERT *;
 
 -- COMMAND ----------
--- MAGIC %sql
--- MAGIC Verify the changes.
+
+-- Verify the changes.
 SELECT
     *
 FROM
